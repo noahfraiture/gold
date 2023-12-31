1ae1f840799be117e74889ecf814d4ccfa854775
Noah <noah.fraiture@gmail.com>
2023-12-31 15:23:18 +0100

    feat: parser and ast copy from monkey

--- repl/repl.go
+++ repl/repl.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"gold/lexer"
-	"gold/token"
+	"gold/parser"
 	"io"
 )
 
@@ -22,9 +22,21 @@ func Start(in io.Reader, out io.Writer) {
 
 		line := scanner.Text()
 		l := lexer.New(line)
+		p := parser.New(l)
 
-		for tok := l.NextToken(); tok.Type != token.EOF; tok = l.NextToken() {
-			fmt.Printf("%+v\n", tok)
+		program := p.ParseProgram()
+		if len(p.Errors()) != 0 {
+			printParserErrors(out, p.Errors())
+			continue
 		}
+
+		io.WriteString(out, program.String()+"\n")
+		io.WriteString(out, "\n")
+	}
+}
+
+func printParserErrors(out io.Writer, errors []string) {
+	for _, msg := range errors {
+		io.WriteString(out, "\t"+msg+"\n")
 	}
 }
